b8d75b542e5101ef0802df31967bf14ab2f29240
wangshengxing <www.sheng.xing@gmail.com>
2020-01-29 16:36:49 +0800

    init project

---
+++ app/src/main/java/com/sen/audio/mp3codecdecoder/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sen.audio.mp3codecdecoder;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
